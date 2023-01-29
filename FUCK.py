2c4ca9e373d958696114b9e182cdcabde68e375c
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2023-01-30 00:09:32 +0600

    Update FUCK.py

--- FUCK.py
+++ FUCK.py
@@ -10,7 +10,7 @@ logo="""
 888        888     888 888        8888888b    
 888        888     888 888    888 888  Y88b   
 888        Y88b. .d88P Y88b  d88P 888   Y88b  
-888         "Y88888P"   "Y8888P"  888    Y88b"""
+888         "Y88888P"   "Y8888P"  888    Y88b XD"""
 print(logo)
 print(45*'-')
 print('[1] JXB BYPASS :)')
@@ -51,4 +51,4 @@ if Fuck in ['3','03']:
 if Fuck in ['4','04']:
     os.system('xdg-open https://www.facebook.com/REMEMBER.THIS.NAME.MR.DIPTO')
 if Fuck in ['0','00']:
-    exit('Bye Bye Idol Take Love From HDX FamilyðŸŒºðŸ¥€')
+    exit('Bye Bye Idol Take Love From HDX Family🌺')
